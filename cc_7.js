ba448a8dc5ca7a12ef0b9362d94d3e134682ae57
Markayla Montgomery <mm42@usf.edu>
2025-02-16 17:41:13 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -8,3 +8,15 @@ function calculateInvoice(subtotal, taxRate, discount) {
 }
 console.log(calculateInvoice(85, 0.08, 10));
 console.log(calculateInvoice(250, 0.1, 15));
+
+// Task 2: Function Expression
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    // Calculate the hourly wage using the formula
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    
+    // Return the hourly wage formatted as a string
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+};
+// Logging the employee's hourly wage
+console.log(calculateHourlyWage(60000, 40));
+console.log(calculateHourlyWage(90000, 30));
